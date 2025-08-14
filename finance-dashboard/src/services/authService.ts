b751ad2bc31648eddedafc540257bef00cbc6f73
s.smaldore <s.smaldore@reply.it>
2025-08-14 17:31:57 +0200

    Aggiunto console log debug

--- finance-dashboard/src/services/authService.ts
+++ finance-dashboard/src/services/authService.ts
@@ -2,6 +2,8 @@ import axios from "axios";
 
 
 export async function login(username: string, password: string) {
+    console.log(import.meta.env.VITE_API_URL)
+
     const res = await axios.post(
         `${import.meta.env.VITE_API_URL}/login`,
         { username, password },
